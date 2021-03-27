255a5693bece9f6a71a11dd4efbf93897450b00e
sean o'connell <s.g.oconnell@gmail.com>
2021-03-27 17:17:37 -0400

    .net on azure?! what a concept!

--- server.js
+++ server.js
@@ -3,22 +3,6 @@ import fs from 'fs';
 import url from 'url';
 import path from 'path';
 
-const http = require('http');
-
-const hostname = '172.23.18.80';
-const port = 443;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Checking....Lunch Time.');
-});
-
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
-
 /**
  * Function to be used as a callback to our http.createServer method
  * It handles incoming requests and sends the response
